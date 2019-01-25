38bae1c5522b0d0894bfa9e6b93faaa3fdf45205
Ota <stevenotaku17@gmail.com>
2019-01-25 10:36:18 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# python-ascii-animation
+A simplie ASCII animation in your terminal using python 
+
+### Who to use :
+
+```git
+git clone https://github.com/nupsco/python-ascii-animation
+```
+```
+cd python-ascii-animation 
+```
+```python 
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,13 @@
+import os , time
+files   = ["data/ascii1.txt", "data/ascii2.txt", "data/ascii3.txt", "data/ascii4.txt", "data/ascii5.txt", "data/ascii6.txt", "data/ascii7.txt", "data/ascii8.txt", "data/ascii9.txt", "data/ascii10.txt"]
+frames  = []
+
+os.system('clear')
+for name in files:
+    with open(name, "r", encoding="utf8") as f:
+        frames.append(f.readlines())
+for i in range(1):
+    for frame in frames:
+        print("".join(frame))
+        time.sleep(0.5)
+        os.system('clear')
