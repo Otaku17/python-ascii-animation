57701aedc5c7bdad9e80d890f7e3f3b372a29011
Ota <stevenotaku17@gmail.com>
2019-04-09 18:25:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os , time
 files = ['data/ascii1.txt', 'data/ascii2.txt', 'data/ascii3.txt', 'data/ascii4.txt', 'data/ascii5.txt', 'data/ascii6.txt', 'data/ascii7.txt', 'data/ascii8.txt', 'data/ascii9.txt', 'data/ascii10.txt']
 frames = []
 
-os.system('cls' if os.name == 'nt' else 'clear')
+os.system('cls||clear')
 for name in files:
     with open(name, 'r', encoding='utf8') as f:
         frames.append(f.readlines())
