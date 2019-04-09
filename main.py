ff299378a7094a0cb0079eaa8a440c34c5166907
Ota <stevenotaku17@gmail.com>
2019-04-09 18:28:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import os , time
 files = ['data/ascii1.txt', 'data/ascii2.txt', 'data/ascii3.txt', 'data/ascii4.txt', 'data/ascii5.txt', 'data/ascii6.txt', 'data/ascii7.txt', 'data/ascii8.txt', 'data/ascii9.txt', 'data/ascii10.txt']
 frames = []
